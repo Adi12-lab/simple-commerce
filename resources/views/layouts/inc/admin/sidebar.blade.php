f1f445b126e1184a69f77505a3fc249655825f1d
Adi Laptop Redmi <adiiskandarubaidah@gmail.com>
2023-08-05 17:25:21 +0700

    Episode 7 edit & delete

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -6,47 +6,54 @@
           <span class="menu-title">Dashboard</span>
         </a>
       </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="pages/forms/basic_elements.html">
+          <i class="mdi mdi-sale menu-icon"></i>
+          <span class="menu-title">Sales</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#category" aria-expanded="false" aria-controls="category">
+          <i class="mdi mdi-view-list menu-icon"></i>
+          <span class="menu-title">Category</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="category">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="{{url("admin/category/create")}}">Add Category</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{url("admin/category")}}">Category</a></li>
+          </ul>
+        </div>
+      </li>
+
       <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-          <i class="mdi mdi-circle-outline menu-icon"></i>
-          <span class="menu-title">UI Elements</span>
+          <i class="mdi mdi-cellphone menu-icon"></i>
+          <span class="menu-title">Products</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="ui-basic">
           <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{url("admin/product/create")}}">Add product</a></li>
+            <li class="nav-item"> <a class="nav-link" href="{{url("admin/product")}}">product</a></li>
           </ul>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/forms/basic_elements.html">
-          <i class="mdi mdi-view-headline menu-icon"></i>
-          <span class="menu-title">Form elements</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/charts/chartjs.html">
-          <i class="mdi mdi-chart-pie menu-icon"></i>
-          <span class="menu-title">Charts</span>
-        </a>
-      </li>
+
+     
       <li class="nav-item">
         <a class="nav-link" href="pages/tables/basic-table.html">
-          <i class="mdi mdi-grid-large menu-icon"></i>
-          <span class="menu-title">Tables</span>
+          <i class="mdi mdi-skype-business menu-icon"></i>
+          <span class="menu-title">Brands</span>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/icons/mdi.html">
-          <i class="mdi mdi-emoticon menu-icon"></i>
-          <span class="menu-title">Icons</span>
-        </a>
-      </li>
+
       <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-          <i class="mdi mdi-account menu-icon"></i>
-          <span class="menu-title">User Pages</span>
+          <i class="mdi mdi-account-multiple menu-icon"></i>
+          <span class="menu-title">Users</span>
           <i class="menu-arrow"></i>
         </a>
         <div class="collapse" id="auth">
@@ -59,11 +66,20 @@
           </ul>
         </div>
       </li>
+      
       <li class="nav-item">
-        <a class="nav-link" href="documentation/documentation.html">
-          <i class="mdi mdi-file-document-box-outline menu-icon"></i>
-          <span class="menu-title">Documentation</span>
+        <a class="nav-link" href="pages/icons/mdi.html">
+          <i class="mdi mdi-emoticon menu-icon"></i>
+          <span class="menu-title">Home Slider</span>
         </a>
       </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="documentation/documentation.html">
+          <i class="mdi mdi-settings menu-icon"></i>
+          <span class="menu-title">Site Setting</span>
+        </a>
+      </li>
+
     </ul>
   </nav>
