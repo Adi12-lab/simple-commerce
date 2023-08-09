aa37c7f154a539711d5783853fa04c10e9f6de77
Adi Laptop Redmi <adiiskandarubaidah@gmail.com>
2023-08-09 16:27:20 +0700

    Ep 27 Wishlist

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Wishlist;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class View extends Component
 {
     public $category, $product, $prodColorSelectedQuantity;
     
+
+    public function addToWishList($productId) {
+        if(Auth::check()) {
+
+            if(Wishlist::where("user_id", auth()->user()->id)->where("product_id", $productId)->exists()) {
+                session()->flash("message", "Already added to wishlist");
+                return false;
+
+            }
+             else {
+                Wishlist::create([
+                     "user_id" => auth()->user()->id,
+                     "product_id" => $productId
+                 ]);
+                session()->flash("message", "Wishlist added successfully");
+                 return true;
+             }
+        } 
+        session()->flash("message", "Please login to continue");
+        return false;
+    }
+
     public function colorSelected($productColorId) {
         // dd($productColorId);
 
